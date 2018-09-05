a4fb5db098659fdcaa2d8bd83033d6250b3da450
Phaninder <phaninder2008@gmail.com>
2018-09-05 10:59:59 +0530

    Attribute routing

---
+++ MVCApp4/MVCApp4/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp4.Controllers
+{
+	[RoutePrefix("HomeController")]
+	public class HomeController : Controller
+    {
+        [Route("HelloWorld/{value:int?}")]
+		public ActionResult About(int value)
+		{
+			return Content("About:"+value);
+		}
+
+        public ActionResult Index(int ?a)
+        {
+            return View();
+        }
+    }
+}
